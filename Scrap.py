9fecc5ee19bed670f395ad45144d03dacbaa5292
Julio Tognetti <julio@tognetti.ar>
2022-11-07 23:40:21 -0300

    Script orsai

---
+++ Scrap.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import re
+import winsound
+import time
+
+geckodriver = 'geckodriver.exe'
+options = Options()
+options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+options.headless = True
+browser = webdriver.Firefox(executable_path=geckodriver, options=options)
+url = "https://suscripcion.orsai.org/"
+browser.get(url)
+delay = 10
+try:
+    element = WebDriverWait(browser, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME , "bg-blue-500"))
+    )      
+    html = browser.page_source
+    soup = BeautifulSoup(html)
+    div = soup.div.div.div
+    text = div.find('div','text-sm mt-4').get_text()
+    monto = div.find('div','text-2xl').get_text()
+
+    cantidad = re.search(r'\d+',text).group(0)
+    print(cantidad)
+    f = open("resutlt.txt","a")
+    f.write(cantidad + " ---> " + monto + " ---> " + time.asctime( time.localtime(time.time()) ) +"\n")
+    if int(cantidad) < 250 :
+        winsound.PlaySound('test.wav', winsound.SND_FILENAME)
+finally:
+    print("QUIT")
+    
+    browser.quit()
+
+
+
---
+++ schedul.bat
@@ -0,0 +1,3 @@
+@echo off
+CD /D F:\Repositorios\ScrapOrsai
+"C:\Users\Cascotito\AppData\Local\Microsoft\WindowsApps\python.exe" "F:\Repositorios\ScrapOrsai\Scrap.py"
